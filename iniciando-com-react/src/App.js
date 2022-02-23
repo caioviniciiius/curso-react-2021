089c123597a90a83e1f71ab001cb402a85b4b289
Caio Vinicius <caio.silva@exporj.com.br>
2022-02-23 15:21:14 -0300

    Criação do projeto cadastro e produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+function App(props) {
+  const modificarNome = (event) => {
+    console.log(event.target.value);
+  };
+
+  const criaComboBox = () => {
+    const opcoes = ["Fulano", "Ciclano"];
+    const comboBoxOpcoes = opcoes.map((opcao) => <option>{opcao}</option>);
+    return <select>{comboBoxOpcoes}</select>;
+  };
+
+  const MeuComboBox = () => criaComboBox();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" value={props.nome} onChange={modificarNome} />
+      <h1>
+        Hello {props.nome} sua idade é {props.idade}
+      </h1>
+      {<MeuComboBox />}
+    </>
   );
 }
 
